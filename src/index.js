aa62babef9807349e67ae2e7b320c8f51357b4d0
MarynaYa <yamarina30@gmail.com>
2022-09-19 13:35:46 +0300

    src-commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/index.scss';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
